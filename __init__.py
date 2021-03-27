6ed73596880836d258097761ea83fa35adf84a40
Genzo <genzo@bk.ru>
2021-03-27 17:29:36 +1000

    ...

--- __init__.py
+++ __init__.py
@@ -1,7 +1,20 @@
+import logzero
+from logzero import logger
+
+
 class ProxyManager:
     """
     Прокси менеджер для скрапперов
     """
 
     def __init__(self):
+        """
+        Конструктор
+        """
         pass
+
+
+if __name__ == "__main__":
+    logzero.loglevel(logzero.DEBUG)
+
+    proxy = ProxyManager()
