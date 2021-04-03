1631b2e0992798b3040410d7549adba6429019bb
Genzo <genzo@bk.ru>
2021-04-03 22:55:24 +1000

    doing tests create and use proxy

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -8,6 +8,8 @@ def test_create_1():
     assert proxy.port == '1234'
     assert proxy.protocol == 'http'
     assert not proxy.anonymity
+    assert proxy.used == 0
+    assert proxy.errors == 0
 
 
 def test_create_2():
@@ -16,10 +18,24 @@ def test_create_2():
     assert proxy.port == '1234'
     assert proxy.protocol == 'http'
     assert not proxy.anonymity
+    assert proxy.used == 0
+    assert proxy.errors == 0
+
+
+def test_create_3():
+    proxy = Proxy('1.1.1.1', '1234', 'https', True)
+    assert proxy.ip == '1.1.1.1'
+    assert proxy.port == '1234'
+    assert proxy.protocol == 'https'
+    assert proxy.anonymity
+    assert proxy.used == 0
+    assert proxy.errors == 0
 
 
 def test_use():
     proxy = Proxy('1.1.1.1', '1234')
+    assert proxy.used == 0
     proxy.use()
-
     assert proxy.used == 1
+    proxy.use()
+    assert proxy.used == 2
