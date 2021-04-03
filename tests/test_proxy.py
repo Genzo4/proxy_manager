6eac7ff209466583f6f54690e13a63dd2ee8ac6a
Genzo <genzo@bk.ru>
2021-04-03 21:40:45 +1000

    create test create and test use

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,1 +1,25 @@
 import pytest
+from proxy_manager.proxy import Proxy
+
+
+def test_create_1():
+    proxy = Proxy('1.1.1.1', '1234')
+    assert proxy.ip == '1.1.1.1'
+    assert proxy.port == '1234'
+    assert proxy.protocol == 'http'
+    assert not proxy.anonymity
+
+
+def test_create_2():
+    proxy = Proxy('1.1.1.1', '1234', 'http', False)
+    assert proxy.ip == '1.1.1.1'
+    assert proxy.port == '1234'
+    assert proxy.protocol == 'http'
+    assert not proxy.anonymity
+
+
+def test_use():
+    proxy = Proxy('1.1.1.1', '1234')
+    proxy.use()
+
+    assert proxy.used == 1
