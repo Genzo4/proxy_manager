d03ba205eab233c5911be5c501a563ce6251f185
Genzo <genzo@bk.ru>
2021-04-04 17:28:46 +1000

    create test_proxy_manager

--- tests/test_proxy_manager.py
+++ tests/test_proxy_manager.py
@@ -1,5 +1,22 @@
 import pytest
+from proxy_manager import ProxyManager
 
 
-def test_add_proxy():
-    assert True
+def test_create_1():
+    pm = ProxyManager(load_fate_proxy=False)
+    assert pm.protocol == 'http'
+    assert not pm.anonymity
+    assert pm.proxy_list == []
+
+
+def test_create_2():
+    pm = ProxyManager('https', True, load_fate_proxy=False)
+    assert pm.protocol == 'https'
+    assert pm.anonymity
+    assert pm.proxy_list == []
+
+
+def test_add_proxy_1():
+    pm = ProxyManager(load_fate_proxy=False)
+    pm._add_proxy('1.2.3.4', '1234', 'http', False)
+    assert pm.proxy_list[0].ip == '1.2.3.4'
