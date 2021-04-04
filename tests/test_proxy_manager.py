35cfa9b60746610c27499d0ab3becbe82fe8cb6f
Genzo <genzo@bk.ru>
2021-04-04 17:32:30 +1000

    test add_proxy

--- tests/test_proxy_manager.py
+++ tests/test_proxy_manager.py
@@ -20,3 +20,33 @@ def test_add_proxy_1():
     pm = ProxyManager(load_fate_proxy=False)
     pm._add_proxy('1.2.3.4', '1234', 'http', False)
     assert pm.proxy_list[0].ip == '1.2.3.4'
+    assert pm.proxy_list[0].port == '1234'
+    assert pm.proxy_list[0].protocol == 'http'
+    assert not pm.proxy_list[0].anonymity
+
+
+def test_add_proxy_2():
+    pm = ProxyManager(load_fate_proxy=False)
+    pm._add_proxy('4.3.2.1', '234', 'https', True)
+    assert pm.proxy_list[0].ip == '4.3.2.1'
+    assert pm.proxy_list[0].port == '234'
+    assert pm.proxy_list[0].protocol == 'https'
+    assert pm.proxy_list[0].anonymity
+
+
+def test_add_proxy_3():
+    pm = ProxyManager(load_fate_proxy=False)
+
+    pm._add_proxy('1.2.3.4', '1234', 'http', False)
+    assert pm.proxy_list[0].ip == '1.2.3.4'
+    assert pm.proxy_list[0].port == '1234'
+    assert pm.proxy_list[0].protocol == 'http'
+    assert not pm.proxy_list[0].anonymity
+
+    pm._add_proxy('4.3.2.1', '234', 'https', True)
+    assert pm.proxy_list[1].ip == '4.3.2.1'
+    assert pm.proxy_list[1].port == '234'
+    assert pm.proxy_list[1].protocol == 'https'
+    assert pm.proxy_list[1].anonymity
+
+
