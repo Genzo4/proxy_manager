f3311b1fa0c32581bfbec02ce99ea17b2b1a9222
Genzo4 <genzo@bk.ru>
2021-04-07 17:15:26 +1000

    debug minitest.py

--- minitest.py
+++ minitest.py
@@ -1,6 +1,7 @@
 from proxy_manager import ProxyManager
+from proxy_manager.consts import PROTOCOL_HTTPS
 
-proxy_list = ProxyManager(protocol="https", anonymity=True)
+proxy_list = ProxyManager(protocol=PROTOCOL_HTTPS, anonymity=True)
 
 for i in range(1, 1000):
     pr = proxy_list.get_random()
