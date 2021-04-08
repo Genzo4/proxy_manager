4c2621a57d324332838591df9dfdd5d5dcc9c7ec
Genzo4 <genzo@bk.ru>
2021-04-08 12:08:04 +1000

    rename proxy_list to proxy_manager

--- minitest.py
+++ minitest.py
@@ -1,8 +1,8 @@
 from proxy_manager_g4 import ProxyManager
 from proxy_manager_g4.consts import PROTOCOL_HTTPS
 
-proxy_list = ProxyManager(protocol=PROTOCOL_HTTPS, anonymity=True)
+proxy_manager = ProxyManager(protocol=PROTOCOL_HTTPS, anonymity=True)
 
 for i in range(1, 1000):
-    pr = proxy_list.get_random()
+    pr = proxy_manager.get_random()
     print(i, ": ", pr, "(", pr.used, ")")
